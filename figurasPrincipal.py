e204c42d7f7f6ce02835eb470bf0d63ecdff38d2
Ariadna Pacheco <ariadnapacheco@MacBook-Air-de-Ariadna-2.local>
2025-11-02 20:31:46 -0600

    Calcularemos las areas de tres figuras con el archivo funciones.py

--- figurasPrincipal.py
+++ figurasPrincipal.py
@@ -1,11 +1,26 @@
 import pandas as pd
 
+from funciones import triangulo, rectangulo, circulo
+
 dataFile = pd.read_csv("figuras.csv")
 
 print("Procesando figuras ...\n")
 
 areas = []
-perimetros = []
+
 
 for index, row in dataFile.iterrows():
-	print(f"Fila {index}: FIGURA={row['FIGURA']}, Medida1={row['MEDIDA1']}, Medida2={row['MEDIDA2']}")
+	print(f"Fila {index}: FIGURA = {row['FIGURA']}, MEDIDA1 = {row['MEDIDA1']}, MEDIDA2 = {row['MEDIDA2']}") 
+	if row['FIGURA']  == "t":
+		area = triangulo(row['MEDIDA1'], row['MEDIDA2'])
+		areas.append(area)
+		
+	elif row['FIGURA']  == "r":
+                area = rectangulo(row['MEDIDA1'], row['MEDIDA2'])
+                areas.append(area)
+
+	elif row['FIGURA']  == "c":
+                area = circulo(row['MEDIDA1'])
+                areas.append(area)
+
+print(f"Las areas de las figuras son: {areas}")
--- funciones.py
+++ funciones.py
@@ -1,16 +1,12 @@
-import math
+def triangulo(base, altura):
+    area = (base * altura) / 2
+    return area
 
-def triangulo(MEDIDA1, MEDIDA2):
-	area = (MEDIDA1 * MEDIDA2) / 2
-	perimetros = MEDIDA1 * 3 
-	return area, perimetros
+def rectangulo(base, altura):
+    area = base * altura
+    return area
 
-def rectangulo(MEDIDA1, MEDIDA2):
-        area = (MEDIDA1 * MEDIDA2)
-        perimetros = 2 * (MEDIDA1 + MEDIDA2)
-        return area, perimetros
+def circulo(radio):
+    area = 3.1416 * (radio ** 2)
+    return area
 
-def circulo(MEDIDA1):
-        area = math.pi * (MEDIDA1 ** 2)
-        perimetros = 2 * * math.pi * MEDIDA1
-        return area, perimetros
